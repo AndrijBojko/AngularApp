bf0a2dd3ea1cf79be1e1da0149a6dc23cd6a24a6
Andrii Boiko <Andrii_Boiko@epam.com>
2018-08-29 13:39:17 +0300

    Add user service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { UserService } from './users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UserService]
 })
 export class AppComponent {
-  title = 'FirstProj';
+  users = [];
+
+  constructor(private userService: UserService){}
+  
+  ngOnInit(){
+    //this.users = this.userService.users
+
+    this.userService.getUsers().subscribe(users => {
+      this.users = users
+    })
+  }
 }
---
+++ src/app/users.service.ts
@@ -0,0 +1,33 @@
+import {Http} from '@angular/http'
+import { Injectable } from '@angular/core';
+import {map} from 'rxjs/operators/'; 
+
+@Injectable()
+export class UserService {
+
+constructor (private http: Http) {}
+
+getUsers(){
+    return this.http.get('https://randomuser.me/api/?inc=gender,name,picture,location&results=8&nat=gb')
+    .pipe(map(response => response.json()))
+    .pipe(map(response => response.results))
+    .pipe(map(users => {
+        return users.map(u => {
+            return {
+                name: u.name.first + ' ' + u.name.last,
+                geo: u.location.city,
+                image: u.picture.large
+            }
+        })
+    }));
+}
+
+    users = [
+        {name: "qwe 1"},
+        {name: "qwe 2"},
+        {name: "qwe 3"},
+        {name: "qwe 4"},  
+        {name: "qwe 5"},  
+        {name: "qwe 5"}
+    ]
+}
